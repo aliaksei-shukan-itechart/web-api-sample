d56d881d14d477caa4f8774120a7230668b200a9
Aliaksei.Shukan <Aliaksei.Shukan@itechart-group.com>
2021-09-22 18:44:32 +0300

    Add initial project and DAL

---
+++ Sample/Sample.DAL/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sample.DAL.Models;
+
+namespace Sample.DAL
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<ToDoItem> ToDoItems { get; set; }
+    }
+}
---
+++ Sample/Sample.DAL/Models/ToDoItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Sample.DAL.Models
+{
+    public class ToDoItem
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime ExpirationTime { get; set; }
+
+        public bool IsCompleted { get; set; }
+    }
+}
